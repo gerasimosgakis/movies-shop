e763a59694bfd467f5c0823984bc6fcbcbf26c42
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2018-10-19 18:44:29 +0100

    feat: create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieListComponent } from './movies/movie-list/movie-list.component';
+import { MovieDetailComponent } from './movies/movie-detail/movie-detail.component';
+import { MovieItemComponent } from './movies/movie-list/movie-item/movie-item.component';
+import { BasketComponent } from './basket/basket.component';
+import { BasketEditComponent } from './basket/basket-edit/basket-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MoviesComponent,
+    MovieListComponent,
+    MovieDetailComponent,
+    MovieItemComponent,
+    BasketComponent,
+    BasketEditComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/basket/basket-edit/basket-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basket-edit',
+  templateUrl: './basket-edit.component.html',
+  styleUrls: ['./basket-edit.component.scss']
+})
+export class BasketEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
