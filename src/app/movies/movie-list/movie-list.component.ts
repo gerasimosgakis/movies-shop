4801627a3cb5c12ee615e21baf816f7aafd2874d
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2018-10-21 17:09:17 +0100

    feat: manage movies in movie service

--- src/app/movies/movie-list/movie-list.component.ts
+++ src/app/movies/movie-list/movie-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Movie } from '../movie.model';
+import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'app-movie-list',
@@ -8,22 +9,12 @@ import { Movie } from '../movie.model';
 })
 export class MovieListComponent implements OnInit {
   @Output() movieWasSelected = new EventEmitter<Movie>();
-  public movies: Movie[] = [
-    new Movie(
-      'Trainspotting',
-      'Drama',
-      'https://img.maximummedia.ie/joe_ie/eyJkYXRhIjoie1widXJsXCI6XCJodHRwOlxcXC9cXFwvbWVkaWEtam9lLm1heGltdW1tZWRpYS5pZS5zMy5hbWF6b25hd3MuY29tXFxcL3dwLWNvbnRlbnRcXFwvdXBsb2Fkc1xcXC8yMDE2XFxcLzAzXFxcLzA0MTU0NzU0XFxcL3RyYWluc3BvdHRpbmcuanBnXCIsXCJ3aWR0aFwiOjc2NyxcImhlaWdodFwiOjQzMSxcImRlZmF1bHRcIjpcImh0dHBzOlxcXC9cXFwvd3d3LmpvZS5pZVxcXC9hc3NldHNcXFwvaW1hZ2VzXFxcL2pvZVxcXC9uby1pbWFnZS5wbmc_dj01XCJ9IiwiaGFzaCI6IjVhZmU1MmI4YTM1YmMxMzZlMGM3ZWYyZWY5YzIwYmFmOWZhMjQ3M2EifQ==/trainspotting.jpg'
-    ),
-    new Movie(
-      'A Clockwork Orange',
-      'Drama/Sci-Fi',
-      'https://i.guim.co.uk/img/media/ac707ddab36726ac4a185b5d64a8b7df867db9b5/0_0_1530_2025/master/1530.jpg?width=700&quality=85&auto=format&fit=max&s=e5b5ff50d81bf622d38fd622769fbd30'
-    )
-  ];
+  public movies: Movie[];;
 
-  constructor() { }
+  constructor(private movieService: MovieService) { }
 
   ngOnInit() {
+    this.movies = this.movieService.getMovies();
   }
 
   public onMovieSelected(movie: Movie) {
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Movie } from './movie.model';
+import { MovieService } from './movie.service';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.scss']
+  styleUrls: ['./movies.component.scss'],
+  providers: [MovieService]
 })
 export class MoviesComponent implements OnInit {
   public selectedMovie: Movie;
