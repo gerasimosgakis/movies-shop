77765a13dc92745409e0146139551505ae1735e7
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2018-10-21 20:12:33 +0100

    feat: retrieve route parameters

--- src/app/movies/movie-edit/movie-edit.component.ts
+++ src/app/movies/movie-edit/movie-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-movie-edit',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-edit.component.scss']
 })
 export class MovieEditComponent implements OnInit {
+  private id: number;
+  public editMode = false;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params;
+          this.editMode = params['id'] != null;
+        }
+      );
   }
 
 }
