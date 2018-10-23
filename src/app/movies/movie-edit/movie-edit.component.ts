04e155c2afa18740ef858e28d5a2cfc07bb479eb
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2018-10-23 20:35:47 +0100

    feat: create form for editing movies

--- src/app/movies/movie-edit/movie-edit.component.ts
+++ src/app/movies/movie-edit/movie-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'app-movie-edit',
@@ -9,8 +11,10 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class MovieEditComponent implements OnInit {
   private id: number;
   public editMode = false;
+  movieForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private movieService: MovieService) { }
 
   ngOnInit() {
     this.route.params
@@ -18,8 +22,28 @@ export class MovieEditComponent implements OnInit {
         (params: Params) => {
           this.id = +params;
           this.editMode = params['id'] != null;
+          this.initForm();
         }
       );
+  }
+
+  private initForm() {
+    let movieTitle = '';
+    let movieImagePath = '';
+    let movieDescription = '';
+
+    if (this.editMode) {
+      const movie = this.movieService.getMovie(this.id);
+      movieTitle = movie.title;
+      movieImagePath = movie.imagePath;
+      movieDescription = movie.description;
+    }
+
+    this.movieForm = new FormGroup({
+      'title': new FormControl(movieTitle),
+      'imagePath': new FormControl(movieImagePath),
+      'description': new FormControl(movieDescription)
+    });
   }
 
 }
