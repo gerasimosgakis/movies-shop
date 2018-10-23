5fa2955fed0fcaf8a941c041c8d7ff86fdd3af7e
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2018-10-23 18:34:39 +0100

    feat: convert the basket form to angular template form

--- src/app/basket/basket-edit/basket-edit.component.ts
+++ src/app/basket/basket-edit/basket-edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Order } from 'src/app/shared/order.model';
 import { BasketService } from '../basket.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basket-edit',
@@ -8,18 +9,15 @@ import { BasketService } from '../basket.service';
   styleUrls: ['./basket-edit.component.scss']
 })
 export class BasketEditComponent implements OnInit {
-  @ViewChild('titleInput') titleInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
 
   constructor(private basketService: BasketService) { }
 
   ngOnInit() {
   }
 
-  public onAddItem() {
-    const orderName = this.titleInputRef.nativeElement.value;
-    const orderAmount = this.amountInputRef.nativeElement.value;
-    const newOrder = new Order(orderName, 10, orderAmount);
+  public onAddItem(form: NgForm) {
+    const value = form.value;
+    const newOrder = new Order(value.title, 10, value.amount);
     this.basketService.addOrder(newOrder);
   }
 
