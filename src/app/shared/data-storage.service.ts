dc226234a4687452ff439d3317308d0cb6b1cfbe
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2018-10-25 20:00:59 +0100

    feat: sending the token

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,19 +3,24 @@ import { Http, Response } from '@angular/http';
 import { MovieService } from "../movies/movie.service";
 import { Movie } from "../movies/movie.model";
 import { map } from 'rxjs/operators';
+import { AuthService } from "../auth/auth.service";
 
 @Injectable()
 export class DataStorageService {
     constructor(private http: Http,
-                private movieService: MovieService) {}
+                private movieService: MovieService,
+                private authService: AuthService) {}
 
     public storeMovies() {
-        return this.http.put('https://movies-32ee8.firebaseio.com/movies.json', this.movieService
+        const token = this.authService.getToken();
+        return this.http.put(`https://movies-32ee8.firebaseio.com/movies.json?auth=${token}`, this.movieService
             .getMovies());
     }
 
     public getMovies() {
-        this.http.get('https://movies-32ee8.firebaseio.com/movies.json')
+        const token = this.authService.getToken();
+            
+        this.http.get(`https://movies-32ee8.firebaseio.com/movies.json?auth=${token}`)
             .pipe(map(
                 (response: Response) => {
                     const movies: Movie[] = response.json();
