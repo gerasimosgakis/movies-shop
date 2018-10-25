f3435dca05e6cf53c52f409acf44bf153a425f27
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2018-10-25 20:41:39 +0100

    feat: add authGuard and redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,14 +7,15 @@ import { MovieDetailComponent } from "./movies/movie-detail/movie-detail.compone
 import { MovieEditComponent } from "./movies/movie-edit/movie-edit.component";
 import { SignupComponent } from "./auth/signup/signup.component";
 import { SigninComponent } from "./auth/signin/signin.component";
+import { AuthGuard } from "./auth/auth-guard.service";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/movies', pathMatch: 'full' },
     { path: 'movies', component: MoviesComponent, children: [
         { path: '', component: MovieStartComponent },
-        { path: 'new', component: MovieEditComponent },
+        { path: 'new', component: MovieEditComponent, canActivate: [AuthGuard] },
         { path: ':id', component: MovieDetailComponent },
-        { path: ':id/edit', component: MovieEditComponent }
+        { path: ':id/edit', component: MovieEditComponent, canActivate: [AuthGuard] }
     ] },
     { path: 'basket', component: BasketComponent },
     { path: 'signup', component: SignupComponent },
