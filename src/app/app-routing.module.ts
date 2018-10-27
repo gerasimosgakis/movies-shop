7a2634d29bfac40ffae5695b526dfd3b42ed61e4
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2018-10-27 14:57:41 +0100

    feat: crteate auth feature module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from '@angular/router';
 import { BasketComponent } from "./basket/basket.component";
-import { SignupComponent } from "./auth/signup/signup.component";
-import { SigninComponent } from "./auth/signin/signin.component";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/movies', pathMatch: 'full' },
-    { path: 'basket', component: BasketComponent },
-    { path: 'signup', component: SignupComponent },
-    { path: 'signin', component: SigninComponent }
+    { path: 'basket', component: BasketComponent }
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,20 +8,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { MovieService } from './movies/movie.service';
 import { HttpModule } from '@angular/http';
 import { DataStorageService } from './shared/data-storage.service';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard.service';
 import { MoviesModule } from './movies/movies.module';
 import { SharedModule } from './shared/shared.module';
 import { BasketModule } from './basket/basket.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    SignupComponent,
-    SigninComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +26,8 @@ import { BasketModule } from './basket/basket.module';
     HttpModule,
     MoviesModule,
     SharedModule,
-    BasketModule
+    BasketModule,
+    AuthModule
   ],
   providers: [
     BasketService,
